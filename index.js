4143df6e2173fd88156a2384cad722ba56e1ca2d
NartRamadan <kataz91@gmail.com>
2025-02-20 13:14:15 +0200

    שרת בסיסי

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 //npm install express body-parser ejs mysql2
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// הגדרת שימוש ב־body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// הגדרת מנוע התבניות (EJS)
+app.set('view engine', 'ejs');
+
+// דוגמה לנתיב ראשוני
+app.get('/', (req, res) => {
+    res.send('ברוך הבא לשרת מעקב לחץ דם!');
+});
+
+// הפעלת השרת על פורט 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
